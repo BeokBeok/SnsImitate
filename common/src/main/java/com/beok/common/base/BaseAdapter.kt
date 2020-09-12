e038b42a2b5b43029b8b90c83305806987319050
BeokBeok <kekemusa37@nate.com>
2020-09-13 00:01:42 +0900

    [추가] BasePagingAdapter

--- common/src/main/java/com/beok/common/base/BaseAdapter.kt
+++ common/src/main/java/com/beok/common/base/BaseAdapter.kt
@@ -2,6 +2,8 @@ package com.beok.common.base
 
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModel
+import androidx.paging.PagedListAdapter
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 
 class BaseAdapter<ITEM : Any>(
@@ -31,5 +33,23 @@ class BaseAdapter<ITEM : Any>(
             clear()
             addAll(items)
         }
+    }
+}
+
+class BasePagingAdapter<ITEM : Any>(
+    private val layoutBindingId: Pair<Int, Int>,
+    private val viewModels: Map<Int, ViewModel>,
+    diffUtil: DiffUtil.ItemCallback<ITEM>
+) : PagedListAdapter<ITEM, BaseViewHolder>(diffUtil) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder =
+        BaseViewHolder(
+            parent,
+            layoutBindingId,
+            viewModels
+        )
+
+    override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
+        holder.bindViewHolder(getItem(position))
     }
 }
