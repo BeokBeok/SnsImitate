6d0d5d26190850037f594ffb224ab15f214b537c
BeokBeok <kekemusa37@nate.com>
2020-09-12 16:05:35 +0900

    [적용] 닉네임 및 패스워드 빈값 입력 시 예외처리

--- app/src/main/java/com/beok/snsimitate/auth/AuthViewModel.kt
+++ app/src/main/java/com/beok/snsimitate/auth/AuthViewModel.kt
@@ -1,6 +1,8 @@
 package com.beok.snsimitate.auth
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.beok.common.model.AuthRequest
@@ -10,6 +12,9 @@ import kotlinx.coroutines.launch
 class AuthViewModel @ViewModelInject constructor(
     private val authRepository: AuthRepository
 ) : ViewModel() {
+
+    private val _errMsg = MutableLiveData<String>()
+    val errMsg: LiveData<String> get() = _errMsg
 
     var isLogin = false
         private set
@@ -24,19 +29,28 @@ class AuthViewModel @ViewModelInject constructor(
 
     private fun doSignUp(nickName: String, password: String, introduce: String) =
         viewModelScope.launch {
-            val result = authRepository.signUp(
-                AuthRequest(
-                    nickname = nickName,
-                    introduction = introduce,
-                    pwd = password
-                )
-            ).getOrNull()
+            val request = AuthRequest(nickname = nickName, introduction = introduce, pwd = password)
+            if (isValidAuthRequest(request)) return@launch
+            val result = authRepository.signUp(request).getOrNull()
             println()
         }
 
+    private fun isValidAuthRequest(request: AuthRequest): Boolean {
+        if (request.isNotValidNickname()) {
+            _errMsg.value = "닉네임이 올바르지 않습니다."
+            return true
+        }
+        if (request.isNotValidPassword()) {
+            _errMsg.value = "패스워드가 올바르지 않습니다."
+            return true
+        }
+        return false
+    }
+
     private fun doSignIn(nickName: String, password: String) = viewModelScope.launch {
-        val result =
-            authRepository.signIn(AuthRequest(nickname = nickName, pwd = password)).getOrNull()
+        val request = AuthRequest(nickname = nickName, pwd = password)
+        if (isValidAuthRequest(request)) return@launch
+        val result = authRepository.signIn(request).getOrNull()
         println()
     }
 }
--- common/src/main/java/com/beok/common/model/AuthRequest.kt
+++ common/src/main/java/com/beok/common/model/AuthRequest.kt
@@ -1,3 +1,8 @@
 package com.beok.common.model
 
-data class AuthRequest(val nickname: String, val introduction: String? = null, val pwd: String)
+data class AuthRequest(val nickname: String, val introduction: String? = null, val pwd: String) {
+
+    fun isNotValidNickname() = nickname.isEmpty()
+
+    fun isNotValidPassword() = pwd.isEmpty()
+}
