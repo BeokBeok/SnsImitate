833be35ac8c9ede0b92b5b8141e737b55123fb88
BeokBeok <kekemusa37@nate.com>
2020-09-12 17:38:04 +0900

    [변경] activity-ktx 버전

--- buildSrc/src/main/java/GradleConfig.kt
+++ buildSrc/src/main/java/GradleConfig.kt
@@ -47,7 +47,7 @@ object JetpackDependencies {
     const val LIFECYCLE_EXT = "androidx.lifecycle:lifecycle-extensions:$LIFECYCLE_VER"
     const val LIFECYCLE_VIEWMODEL_KTX = "androidx.lifecycle:lifecycle-viewmodel-ktx:$LIFECYCLE_VER"
 
-    const val ACTIVITY_KTX = "androidx.activity:activity-ktx:1.2.0-alpha07"
+    const val ACTIVITY_KTX = "androidx.activity:activity-ktx:1.2.0-alpha03"
 
     const val PREFERENCE_KTX = "androidx.preference:preference-ktx:1.1.1"
 }
