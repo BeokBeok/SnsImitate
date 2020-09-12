4082ce364cc986c9a733bfa4c53635d15bcb142b
BeokBeok <kekemusa37@nate.com>
2020-09-12 20:38:33 +0900

    [추가] BaseFragment

---
+++ common/src/main/java/com/beok/common/base/BaseFragment.kt
@@ -0,0 +1,26 @@
+package com.beok.common.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<VDB : ViewDataBinding>(
+    @LayoutRes private val layoutId: Int
+) : Fragment() {
+
+    protected lateinit var binding: VDB
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = DataBindingUtil.inflate<VDB>(inflater, layoutId, container, false)
+        .apply { lifecycleOwner = viewLifecycleOwner }
+        .also { binding = it }
+        .root
+}
