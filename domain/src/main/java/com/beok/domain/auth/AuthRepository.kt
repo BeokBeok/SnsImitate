7a37b37df66a4335aa26ce5c0272631d6f0df1d4
BeokBeok <kekemusa37@nate.com>
2020-09-12 18:44:14 +0900

    [수정] api 호출 시 ioDispatcher 사용

--- domain/src/main/java/com/beok/domain/auth/AuthRepository.kt
+++ domain/src/main/java/com/beok/domain/auth/AuthRepository.kt
@@ -2,15 +2,23 @@ package com.beok.domain.auth
 
 import com.beok.common.model.AuthRequest
 import com.beok.domain.auth.entity.AuthResponse
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class AuthRepository @Inject constructor(
     private val authService: AuthService
 ) : AuthDataSource {
 
+    private val ioDispatcher = Dispatchers.IO
+
     override suspend fun signUp(authRequest: AuthRequest): Result<AuthResponse> =
-        runCatching { authService.requestSignUp(authRequest) }
+        withContext(ioDispatcher) {
+            runCatching { authService.requestSignUp(authRequest) }
+        }
 
     override suspend fun signIn(authRequest: AuthRequest): Result<AuthResponse> =
-        runCatching { authService.requestSignIn(authRequest) }
+        withContext(ioDispatcher) {
+            runCatching { authService.requestSignIn(authRequest) }
+        }
 }
